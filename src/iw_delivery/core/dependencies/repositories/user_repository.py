5ec4eb916505291cc5f4e9c2437061bdba44f06e
i-walk-away <orchidfake999@yandex.ru>
2026-06-26 15:31:14 +0300

    DI for user repostory

---
+++ src/iw_delivery/core/dependencies/repositories/user_repository.py
@@ -0,0 +1,14 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.iw_delivery.core.dependencies.db import get_session
+from src.iw_delivery.repositories.db.users import UserRepository
+
+
+def get_user_repository(
+        session: AsyncSession = Depends(get_session)
+) -> UserRepository:
+    """
+    Constructs a UserRepository instance with injected AsyncSession.
+    """
+    return UserRepository(session)
